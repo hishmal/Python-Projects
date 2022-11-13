fa4f9ba89a05344d060df20d1f851e13bec4a142
Hisham Malik <114481818+hishmal@users.noreply.github.com>
2022-11-13 15:26:07 -0500

    Changes.

---
+++ Sending an Email with Python/sendmail.py
@@ -0,0 +1,10 @@
+import smtplib
+
+server = smtplib.SMTP('smtp.gmail.com',587)
+
+server.starttls()
+
+server.login('zahid.tnear@gmail.com', 'pythonPractice')
+
+server.sendmail('zahid.tnear@gmail.com','hishymalik@gmail.com','Hey,\nThis is a test.')
+print('Mail Sent')
