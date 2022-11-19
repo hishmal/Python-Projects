6bfe6d853c2173db2dca9afb8fe1fefe77db8b1d
Hisham Malik <114481818+hishmal@users.noreply.github.com>
2022-11-19 13:04:54 -0500

    Delete Sorting Practice with Python directory

--- Sorting Practice with Python/Zip Function.py
+++
@@ -1,8 +0,0 @@
-names = ("Hisham", "Kiran", "Harsh")
-companies = ("Dell", "Apple", "Microsoft")
-
-print(list(zip(names, companies)))
-
-#zipped = zip(names, companies)
-#for names, companies in zipped:
-    #print(companies, names)
